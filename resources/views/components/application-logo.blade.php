d8c9b5b7d2cd18bf6ce498eec00d6b54c63cbe11
Erik Vladár <125514263+ErikVladar@users.noreply.github.com>
2026-03-03 00:28:20 +0100

    auth screen edit

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'flex items-center justify-center bg-stone-600 rounded-full']) }}>
+<div {{ $attributes->merge(['class' => 'flex items-center justify-center ']) }}>
     <img src="{{ asset('logo-korza.png') }}" alt="Logo" class="p-1 w-full h-full object-contain" />
 </div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -35,11 +35,12 @@
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-stone-800">
         <div>
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <x-application-logo class="w-56 h-56 fill-current text-gray-500" />
             </a>
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-stone-700 text-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-stone-700 text-white shadow-md overflow-hidden sm:rounded-lg"
+                    style="border-radius: 0 0 2.5rem 2.5rem;">
             {{ $slot }}
         </div>
     </div>
