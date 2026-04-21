06f93de8f4eb7c16cd62a22f3a194d4405874b01
Erik Vladár <125514263+ErikVladar@users.noreply.github.com>
2026-04-21 22:26:19 +0200

    favicon fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,6 @@
     <title>Na Korze</title>
 
     <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
